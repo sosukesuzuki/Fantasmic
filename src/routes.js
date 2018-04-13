5cb43792381171bc8c4d1ab7f1881eb8bb772fc7
Sosuke Suzuki <aosukeke@gmail.com>
2018-04-14 01:58:04 +0900

    add park url

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,28 @@
 import Router from 'express-promise-router'
+import themeparks from 'themeparks'
 
 const router = Router()
 
 router.get('/', (req, res) => {
   res.render('home')
+})
+
+router.get('/:park', async (req, res) => {
+  const { park } = req.params
+  const disneyPark = park === 'sea'
+    ? new themeparks.Parks.TokyoDisneyResortDisneySea()
+    : park === 'land'
+      ? new themeparks.Parks.TokyoDisneyResortMagicKingdom()
+      : ''
+
+  if (disneyPark === '') res.json({error: 'invalid park name.'})
+
+  const waitTimes = await disneyPark.GetWaitTimes()
+  const times = await disneyPark.GetOpeningTimes()
+  const parkName = park === 'sea'
+    ? 'シー'
+    : 'ランド'
+  res.render('park', {parkName, waitTimes, times})
 })
 
 export default router
